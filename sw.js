0b2df697d4bedcb1ccd3ad45f6de3d66313fb94e
davenn <davennmannix@gmail.com>
2026-05-09 14:29:16 -0500

    Big update - PWA for local installing

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'davenn-v1';
+const SHELL = [
+  '/index.html',
+  '/tracktimer.html',
+  '/meetingtimer.html',
+  '/toolshare.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Always go to network for API calls
+  if (e.request.url.includes('/api.php')) return;
+
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if (res && res.status === 200 && e.request.method === 'GET') {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
+});
